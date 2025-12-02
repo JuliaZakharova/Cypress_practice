bc65dd542b876033fb4ab5361b78fde2667f7401
Julia Zakharova <yukasiitake@gmail.com>
2025-12-02 23:43:30 +0300

    8+2

--- cypress/e2e/firstTest.cy.js
+++ cypress/e2e/firstTest.cy.js
@@ -1,13 +1,4 @@
-<<<<<<< HEAD
 /// <reference types="Cypress" /> 
 
 
-
-//ttt+2+3
-=======
-/// <reference types="Cypress" /> 
-
-
-
-//ttt+2
->>>>>>> e415c60f6ed93989c9784a350870d6106864aea5
+//8+2
