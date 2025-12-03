9ca7d1021939c4009b95bb6b15325701a291322e
Julia Zakharova <yukasiitake@gmail.com>
2025-12-03 23:25:29 +0300

    update

--- cypress/e2e/firstTest.cy.js
+++ cypress/e2e/firstTest.cy.js
@@ -1,4 +1,12 @@
 /// <reference types="Cypress" /> 
 
 
-//8+2
+it('SHOULD', ()=> {
+    cy.visit('https://cash.rbc.ru/converter.html')
+    cy.get('[class="calc__input_box__input js-converter-input"]').eq(0)
+        .click()
+        .type('{selectall}{backspace}')   // выделяем и удаляем предыдущее значение
+        .type(100)  //команда вводит какое-то значение в инпут
+        .should('have.value', 100) //проверка, что поменялось значение на указанное (дефолтно стоит 5000)
+        .and('be.visible')  //проверка, чтобы инпут визибл
+})
