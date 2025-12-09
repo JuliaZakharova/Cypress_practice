c782aac6b0fb429308406c8610a262a841c46416
Julia Zakharova <yukasiitake@gmail.com>
2025-12-09 22:54:59 +0300

    add part 1

--- cypress/e2e/firstTest.cy.js
+++ cypress/e2e/firstTest.cy.js
@@ -1,3 +1,24 @@
 /// <reference types="Cypress" /> 
 
+//type()
+it('type', ()=>{
+    cy.visit('https://aqa-proka4.org/sandbox/web')
+        .get('[name="phone[]"]')
+        .type(73334445577)
+})
 
+//focus()
+it('focus', ()=>{
+    cy.visit('https://aqa-proka4.org/sandbox/web')
+        .get('[name="dyn-name"]')
+        .focus()
+})
+
+//blur() 
+it.only('blur', ()=>{
+    cy.visit('https://followish.io/auth/login')
+        .get('[name="email"]')
+        .type(1) //сработал блюр только при неверном вводе в инпут, 
+                // как пишут в примерах с focus не сработал блюр
+        .blur()
+})
