c0a3ac4c73af61e2e317c5363b41bf8cdd165fb8
Julia Zakharova <yukasiitake@gmail.com>
2026-01-28 23:43:15 +0300

    new lessons

--- cypress/e2e/firstTest.cy.js
+++ cypress/e2e/firstTest.cy.js
@@ -1,48 +1,38 @@
 /// <reference types="Cypress" /> 
 
+//import { archivePage } from "../support/pages/archive"
 
-it('SHOULD', ()=> {
-    cy.visit('https://cash.rbc.ru/converter.html')
-    cy.get('[class="calc__input_box__input js-converter-input"]').eq(0)
-        .click()
-        .type('{selectall}{backspace}')   // выделяем и удаляем предыдущее значение
-        .type(100)  //команда вводит какое-то значение в инпут
-        .should('have.value', 100) //проверка, что поменялось значение на указанное (дефолтно стоит 5000)
-        .and('be.visible')  //проверка, чтобы инпут визибл
+//пример не рабочий, просто пример
+
+//mock
+//beforeEach("setup success response with stub", ()=> {
+//    cy.intercept("https...",
+//       {fixture: "confirmResponse/success.json"})
+//})
+
+//stub
+//beforeEach("setup success response with stub", ()=> {
+//    cy.intercept("https...",
+//        {fixture: "archiveResponse/success.json"})
+//})
+
+it("check state of paymant in the archive | publick session", () => {
+    cy.intercept("https...",
+        {fixture: "archiveResponse/success.json"
+
+    });
+    basePage.open("url");
+    archivePage.selectArchiveMenu()
 })
 
-it('EXPECT', ()=> {
-    cy.visit('https://cash.rbc.ru/converter.html')
-    cy.get('[class="calc__input_box__input js-converter-input"]').eq(0)
-        .click()
-        .type('{selectall}{backspace}')
-        .type(100).then(input=> {
-            expect(input).to.have.value(100)
-        })
-        
-})
-
-it('дефолтное состояние cheked в чекбоксе', ()=> {
-    cy.visit('https://the-internet.herokuapp.com/checkboxes')
-    cy.get('[type="checkbox"]').eq(1)
-        .should('be.checked')
-})
-
-it('проверка видимости вложенной ссылки', ()=> {
-    cy.visit('https://the-internet.herokuapp.com/jqueryui/menu#') //переходим на опред. ресурс
-    cy.contains('Enabled') //найдем эл-т с текстом
-        .trigger('mouseover') //наведем мышью на него
-        .get('#ui-id-4') //проверим что эл-т с таким id есть
-        .should('be.visible') //и он виден
-})
-
-it('элемент имеет атрибут и имеет конкретное значение', ()=> {
-    cy.visit('https://the-internet.herokuapp.com/upload')
-    cy.get('#file-submit')
-        .should('have.attr', 'type').and('match', /submit/) //матч это должен совпадать, /с этим значением/
-})
-
-it.only('проверка корретности url', ()=> {
-    cy.visit('https://the-internet.herokuapp.com/upload')
-    cy.url().should('eq','https://the-internet.herokuapp.com/upload')
-})
+describe('Проверка плагина', () => {
+  it.only('Делает первый скриншот', () => {
+    // Заходим на любой стабильный сайт
+    cy.visit('https://google.com'); 
+    
+    // Делаем снимок. 
+    // При ПЕРВОМ запуске плагин просто создаст файл-эталон.
+    // При ВТОРОМ запуске он начнет сравнивать.
+    cy.matchImageSnapshot('test-google');
+  });
+});
--- cypress/support/e2e.js
+++ cypress/support/e2e.js
@@ -15,3 +15,26 @@
 
 // Import commands.js using ES2015 syntax:
 import './commands'
+
+// cypress.config.ts
+
+import {defineConfig} from 'Cypress'
+import {addMatchImageSnapshotPlugin} from '@simonsmith/cypress-image-snapshot/plugin'
+
+export default defineConfig({
+  e2e: {
+    setupNodeEvents(on) {
+      addMatchImageSnapshotPlugin(on)
+    },
+  },
+})
+
+import {addMatchImageSnapshotCommand} from '@simonsmith/cypress-image-snapshot/command'
+
+addMatchImageSnapshotCommand()
+
+// can also add any default options to be used
+// by all instances of `matchImageSnapshot`
+addMatchImageSnapshotCommand({
+  failureThreshold: 0.2,
+})
